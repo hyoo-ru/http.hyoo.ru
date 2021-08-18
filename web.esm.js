1c191e3b131e3aaed48990c7ea72fe5059b4dc4d
nin-jin <nin-jin@users.noreply.github.com>
2021-08-18 07:20:28 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -6562,7 +6562,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    class $mol_grid extends $.$mol_scroll {
+    class $mol_grid extends $.$mol_view {
         row_height() {
             return 32;
         }
