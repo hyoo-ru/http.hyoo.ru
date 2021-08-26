0c2604a8f8df9585d7d4eda12c26d8f45f638bdf
nin-jin <nin-jin@users.noreply.github.com>
2021-08-26 07:22:44 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.js
+++ web.js
@@ -4485,9 +4485,6 @@ var $;
         '@': {
             mol_link_current: {
                 'true': {
-                    background: {
-                        color: $.$mol_theme.hover,
-                    },
                     color: $.$mol_theme.text,
                     textShadow: '0 0',
                 }
