b4d27980762de96f1f5a92cf54eec4199b03419f
nin-jin <nin-jin@users.noreply.github.com>
2023-02-05 07:16:12 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1103,12 +1103,6 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    class $mol_media extends $mol_object2 {
-        static match(query: string, next?: boolean): boolean;
-    }
-}
-
-declare namespace $ {
     function $mol_lights(this: $, next?: boolean): boolean;
 }
 
@@ -2032,13 +2026,7 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    class $mol_icon_clipboard_arrow_up extends $mol_icon {
-        path(): string;
-    }
-}
-
-declare namespace $ {
-    class $mol_icon_clipboard_arrow_up_outline extends $mol_icon {
+    class $mol_icon_clipboard_outline extends $mol_icon {
         path(): string;
     }
 }
@@ -2047,7 +2035,7 @@ declare namespace $ {
     class $mol_button_copy extends $mol_button_minor {
         text(): string;
         sub(): readonly any[];
-        Icon(): $mol_icon_clipboard_arrow_up_outline;
+        Icon(): $mol_icon_clipboard_outline;
     }
 }
 
