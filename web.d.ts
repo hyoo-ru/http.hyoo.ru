ecaccbb5499ca660fe3e5bf0f97cc638f908e117
nin-jin <nin-jin@users.noreply.github.com>
2023-02-07 07:17:13 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -1643,6 +1643,7 @@ declare namespace $ {
         line_height(): number;
         letter_width(): number;
         width_limit(): number;
+        row_width(): number;
         sub(): readonly any[];
     }
 }
